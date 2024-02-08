1e8bb3ae2bea1b308aea7bd1170c166d4db2b625
Marcos Santos <marcossantosmsms604@gmail.com>
2024-02-08 19:40:08 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(2, "livros");
-		System.out.println(dp);
+		
+		Seller seller = new Seller(1, "Marcos", "marcos@gmail.com", new Date(), 3500.0, dp);
+		
+		System.out.println(seller);
 	}
 
 }
